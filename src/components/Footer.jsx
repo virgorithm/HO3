011ed696591c76e2855c640bb609a75b14ccc552
Imari Calado <imarikristel@gmail.com>
2025-05-13 04:03:04 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,7 @@ const Footer = () => {
       <p>© {new Date().getFullYear()} Walang laman dito. Parang wallet ko pag petsa de peligro.
 
 </p>
-      <div className="footer-links">
-        <a href="https://github.com/virgorithm" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://ph.pinterest.com/pin/120260252545590594/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://www.facebook.com/">Facebook</a>
-      </div>
+      
     </footer>
   );
 };
